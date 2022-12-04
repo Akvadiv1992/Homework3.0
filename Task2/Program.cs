858f9eeaa9561b460e021b37927a8b30fd2f3dd6
IGOR <sahmen@mail.ru>
2022-12-05 01:45:37 +0300

    DZ 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координаты точек А: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+double ya = Convert.ToDouble(Console.ReadLine());
+double za = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точек B: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+double yb = Convert.ToDouble(Console.ReadLine());
+double zb = Convert.ToDouble(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
+Console.WriteLine($"Расстояние между точками: {Math.Round(res, 3)}");
